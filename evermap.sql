9b3e726c3b5350adfbc4562be2b51fea707bfdd8
Lilahamstern <leo.ronnebro@gmail.com>
2020-01-03 12:53:55 +0100

    Modifed enum

--- evermap.sql
+++ evermap.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `markers` (
   `id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL UNIQUE,
   `label` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL UNIQUE,
   `owned_by` int(10) NOT NULL,
-  `type` ENUM('shop', 'base') NOT NULL,
+  `type` ENUM('SHOP', 'BASE') NOT NULL,
   `verified` boolean NOT NULL,
   `verified_by` int (10),
   KEY `fk_markers_owned_by_idx` (`owned_by`),
