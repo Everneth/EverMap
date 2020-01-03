70b4acb54926911f612710c19ac79ae01f06dd51
Lilahamstern <leo.ronnebro@gmail.com>
2020-01-03 13:56:24 +0100

    Inserting marker on add

--- src/main/java/com/everneth/evermap/Commands/Marker.java
+++ src/main/java/com/everneth/evermap/Commands/Marker.java
@@ -2,6 +2,11 @@ package com.everneth.evermap.commands;
 
 import com.everneth.evermap.App;
 import com.everneth.evermap.manager.DynmapManager;
+import com.everneth.evermap.models.EMIPlayer;
+import com.everneth.evermap.models.MarkerModel;
+import com.everneth.evermap.models.MarkerType;
+import com.everneth.evermap.utils.PlayerUtils;
+import com.everneth.evermap.utils.Utils;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -22,7 +27,12 @@ public class Marker extends BaseCommand {
   @Subcommand("add")
   public void onAdd(CommandSender sender, String label) {
     Player player = (Player) sender;
-    dm.addMarker(player, label);
+    String markerID = dm.addMarker(player, label);
+    EMIPlayer emiPlayer = PlayerUtils.getPlayer(sender.getName());
+    MarkerModel markerModel = new MarkerModel(markerID, label, emiPlayer, MarkerType.BASE, true, emiPlayer);
+    markerModel.Insert();
+
+    player.sendMessage(Utils.color("&3 Marker created" + "&4 " + MarkerType.BASE));
   }
 
   @Subcommand("remove")
