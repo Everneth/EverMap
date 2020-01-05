a9e42511d3c9512af8968231c56988ccca2ae0aa
lilahamstern <leo.ronnebro@gmail.com>
2020-01-05 08:50:36 +0100

    Tinyint insteed of bool

--- evermap.sql
+++ evermap.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `markers` (
   `label` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL UNIQUE,
   `owned_by` int(10) NOT NULL,
   `type` ENUM('SHOP', 'BASE') NOT NULL,
-  `verified` boolean NOT NULL,
+  `verified` tinyint(4) NOT NULL,
   `verified_by` int (10),
   KEY `fk_markers_owned_by_idx` (`owned_by`),
   KEY `fk_markers_verified_by_idx` (`verified_by`),
